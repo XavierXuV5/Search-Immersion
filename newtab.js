31a8ddc89b71252f3401482cc73e541fa6aef75e
Naikaku <naikaku00@gmail.com>
2026-01-10 15:20:04 +0900

    Delete newtab.js

--- newtab.js
+++
@@ -1,9 +0,0 @@
-chrome.storage.local.get(['redirectNewTab'], (result) => {
-  
-  const isEnabled = result.redirectNewTab !== false;
-
-
-  if (isEnabled) {
-    window.location.href = "https://www.google.com/";
-  }
-});
